ae536401ef79264001719fda740cd84656d6f0b8
cosmas <cosmas@localhost.localdomain>
2022-11-29 13:22:40 +0300

    changes

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-print("hello world")
 import random
-
 def shuffle(string):
     tempList=list(string)
     random.shuffle(tempList)
     return ''.join(tempList)
-
-
-
-uppercaseletter1=chr(random.randint(65, 90))
-uppercaseletter2=chr(random.randint(65, 90))
-
+uppercaseletter1=chr(random.randint(50, 90))
+uppercaseletter2=chr(random.randint(50, 90))
+uppercaseletter2=chr(random.randint(50, 90))
+uppercaseletter2=chr(random.randint(50, 90))
+uppercaseletter2=chr(random.randint(50, 90))
 password=uppercaseletter1 + uppercaseletter2
 password=shuffle(password)
 print('Ouch! you want password,,,cool here is it' + " " +password)
